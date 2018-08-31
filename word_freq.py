539c599bf5bf736d1c13629add9fa40f01b9e4f1
Brian Waite <waiteb15@gmail.com>
2018-08-31 09:08:47 -0400

    word_freq

--- word_freq.py
+++ word_freq.py
@@ -5,14 +5,15 @@ import sys
 all_words={}
 file_name = sys.argv[1]
 
-rx_wordsep = re.compile(r"[^\w']+",re.I)
+rx_wordsep = re.compile(r"[^\w']+")
 
 with open(file_name) as file_in:
-    words = rx_wordsep.split(file_in.read())
-    for word in words:
-        if word == '' or word == "'":
-            continue
-        all_words[word.lower()] = all_words.get(word.lower(),0) + 1
+    for line in file_in:
+        words = rx_wordsep.split(line.strip())
+        for word in words:
+            if word == '' or word == "'":
+                continue
+            all_words[word.lower()] = all_words.get(word.lower(),0) + 1
 
 
 for word, count in sorted(all_words.items()):
